64554fafe68f90a166d3f1c0041d8761e87bb3ff
Ching Pei Yang <badnam3o.0@gmail.com>
2023-10-24 04:02:49 +0200

    sql: add query GetUser and regenerate

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -42,3 +42,23 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 	)
 	return i, err
 }
+
+const getUser = `-- name: GetUser :one
+SELECT id, created_at, updated_at, name, apikey
+FROM users
+WHERE apiKey = $1
+LIMIT 1
+`
+
+func (q *Queries) GetUser(ctx context.Context, apikey string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUser, apikey)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Name,
+		&i.Apikey,
+	)
+	return i, err
+}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -2,3 +2,9 @@
 INSERT INTO users (id, created_at, updated_at, name)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
+
+-- name: GetUser :one
+SELECT *
+FROM users
+WHERE apiKey = $1
+LIMIT 1;
