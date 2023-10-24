9bded87569116e6b50b374f52c8ab04124b64a2e
Ching Pei Yang <badnam3o.0@gmail.com>
2023-10-25 00:30:50 +0200

    fix: roll back schema and add delete cascade
    couldn't find out how to add a ON DELETE CASCADE constraint, so I just nuked it

--- sql/schema/004_feed_follows.sql
+++ sql/schema/004_feed_follows.sql
@@ -3,8 +3,10 @@ CREATE TABLE feed_follows (
 	id UUID NOT NULL PRIMARY KEY,
 	user_id UUID NOT NULL,
 	feed_id UUID NOT NULL,
-	FOREIGN KEY(user_id) REFERENCES users(id),
-	FOREIGN KEY(feed_id) REFERENCES feeds(id)
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
 );
 
 
--- sql/schema/005_feed_follow_timestamps.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
-ALTER TABLE feed_follows
-ADD created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-ADD updated_at TIMESTAMP NOT NULL DEFAULT NOW();
-
--- +goose Down
-ALTER TABLE feed_follows
-DROP created_at, updated_at;
