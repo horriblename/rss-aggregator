5d648ab6e514f1508ad24dfab87416ed14c97e8b
Ching Pei Yang <badnam3o.0@gmail.com>
2023-10-25 03:12:03 +0200

    sql: add table posts

---
+++ sql/schema/006_posts.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE posts (
+	id UUID NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	title VARCHAR(127) NOT NULL,
+	url VARCHAR(127)  NOT NULL,
+	description VARCHAR(255),
+	published_at TIMESTAMP NOT NULL,
+	feed_id UUID NOT NULL,
+	FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
