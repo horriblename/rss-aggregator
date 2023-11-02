4a953e92836d8bf05fc8330ff4618d682cd4ec76
Ching Pei Yang <badnam3o.0@gmail.com>
2023-11-02 04:25:15 +0100

    feat(frontend): add fetch follows

--- webclient/src/Feed.elm
+++ webclient/src/Feed.elm
@@ -1,4 +1,4 @@
-module Feed exposing (Feed, createFeed, fetchFeeds)
+module Feed exposing (Feed, UUID, createFeed, fetchFeeds, fetchFollows, followFeed)
 
 import Http exposing (header)
 import Json.Decode as Decode exposing (Decoder, list, string)
@@ -55,6 +55,57 @@ createFeed apiKey toMsg params =
         , url = apiBaseUrl ++ "/v1/feeds"
         , body = Http.jsonBody (encodeCreateFeedParams params)
         , expect = Http.expectJson toMsg feedDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+type alias UUID =
+    String
+
+
+
+-- the API returns more data but this is all we need
+
+
+type alias FetchFollows =
+    { id : String }
+
+
+
+-- fetches a list of follows as UUIDs
+
+
+fetchFollows : String -> (Result Http.Error (List UUID) -> msg) -> Cmd msg
+fetchFollows apiKey toMsg =
+    let
+        decoder =
+            Decode.succeed FetchFollows
+                |> required "id" string
+    in
+    Http.request
+        { method = "GET"
+        , headers = [ header "Authorization" <| "ApiKey " ++ apiKey ]
+        , url = apiBaseUrl ++ "/v1/feeds"
+        , body = Http.emptyBody
+        , expect = Http.expectJson toMsg (Decode.map (List.map .id) (list decoder))
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+followFeed : String -> String -> (Result Http.Error () -> msg) -> Cmd msg
+followFeed apiKey feedID toMsg =
+    let
+        params =
+            Encode.object [ ( "feed_id", Encode.string feedID ) ]
+    in
+    Http.request
+        { method = "POST"
+        , headers = [ header "Authorization" <| "ApiKey " ++ apiKey ]
+        , url = apiBaseUrl ++ "/v1/feed_follows"
+        , body = Http.jsonBody params
+        , expect = Http.expectWhatever toMsg
         , timeout = Nothing
         , tracker = Nothing
         }
