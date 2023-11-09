520ea59c2d9521d4368bf6171c5627fad3e09eb3
Ching Pei Yang <badnam3o.0@gmail.com>
2023-11-09 19:14:47 +0100

    fix(deploy): workaround TLS issues by building on alpine

--- flake.nix
+++ flake.nix
@@ -28,11 +28,13 @@
         dockerTools.streamLayeredImage {
           name = "rss-aggre";
           tag = "latest";
-          # fromImage = dockerTools.pullImage {
-          #   imageName = "alpine";
-          #   imageDigest = "sha256:f3334cc04a79d50f686efc0c84e3048cfb0961aba5f044c7422bd99b815610d3";
-          #   sha256 = "sha256-snYCbJocC3VLcVvOJzlujtHcJAHJHExhxoq/9r3yYvI=";
-          # };
+
+          # I don't wanna deal with TLS certs so I'm stealing them from alpine :p
+          fromImage = dockerTools.pullImage {
+            imageName = "alpine";
+            imageDigest = "sha256:f3334cc04a79d50f686efc0c84e3048cfb0961aba5f044c7422bd99b815610d3";
+            sha256 = "sha256-snYCbJocC3VLcVvOJzlujtHcJAHJHExhxoq/9r3yYvI=";
+          };
 
           # copyToRoot = buildEnv {
           #   name = "rss-aggre";
@@ -55,6 +57,11 @@
               # formatted as a psql connection string
               # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
               ''DATABASE_URL="user=rss-aggre host=localhost port=5432"''
+
+              # Path to file containing db password; can be used alongside DATABASE_URL
+              # This is intended to be used in conjunction with docker secrets
+              # example: "/run/secrets/db_password.txt"
+              "DATABASE_PASSWORD_FILE=''"
             ];
             Entrypoint = [
             ];
